7fd2f0b97f41ebe89fc41c816b2c9303f0301ee2
devyatharth0 <devyatharth4@gmail.com>
2024-11-30 17:11:37 +0530

    Initial commit

---
+++ background.js
@@ -0,0 +1,7 @@
+/*
+ * Yathome
+ * Copyright (c) 2023-2024 Yatharth
+ * Licensed under the GNU General Public License v3.0 (GPL-3.0)
+ * You should have received a copy of the GNU General Public License along with this program.
+ * If not, see <https://www.gnu.org/licenses/>.
+ */
